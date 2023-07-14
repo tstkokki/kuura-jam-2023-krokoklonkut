38a3794be7edfc53f1f0cccf8715b15741784f61
tstkokki <60537602+tstkokki@users.noreply.github.com>
2023-07-14 20:44:00 +0300

    Game event

---
+++ KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Game Event/GameEvent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Game Event", menuName = "Custom/Game Event")]
+public class GameEvent : ScriptableObject
+{
+    List<GameEventListener> listeners = new List<GameEventListener>();
+
+
+    public void Raise()
+    {
+        for (int i = listeners.Count-1; i > 0; i--)
+        {
+            listeners[i].Invoke();
+        }
+    }
+
+    public void Add(GameEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void Remove(GameEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
