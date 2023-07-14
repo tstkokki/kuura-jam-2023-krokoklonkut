4be910b59ca90ae081d39516d21fe9b77c8e03eb
tstkokki <60537602+tstkokki@users.noreply.github.com>
2023-07-14 21:32:21 +0300

    Update Sauna.cs

--- KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Sauna.cs
+++ KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Sauna.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class Sauna : ScriptableObject
 {
     public List<SaunaUkko> UkkoList = new List<SaunaUkko>();
