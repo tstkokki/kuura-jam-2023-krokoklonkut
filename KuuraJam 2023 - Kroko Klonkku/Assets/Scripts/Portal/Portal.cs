630beb6b013efd729fd2c6b0ccf826b9b7634848
tstkokki <60537602+tstkokki@users.noreply.github.com>
2023-07-15 14:54:46 +0300

    asetetaan portaali

---
+++ KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    [SerializeField]
+    List<Vector3Variable> portaalit = new List<Vector3Variable>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void Siirry(int portaali)
+    {
+        Debug.Log($"Siirry {portaali}");
+        transform.position = portaalit[portaali >= 0 && portaali < portaalit.Count ? portaali : 0].Position;
+    }
+}
