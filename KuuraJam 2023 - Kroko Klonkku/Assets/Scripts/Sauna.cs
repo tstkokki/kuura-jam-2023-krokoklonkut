9bb2fd1e2044f9cb96ee464174ab3c6cb86413e1
tstkokki <60537602+tstkokki@users.noreply.github.com>
2023-07-15 20:40:25 +0300

    Kiuas

--- KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Sauna.cs
+++ KuuraJam 2023 - Kroko Klonkku/Assets/Scripts/Sauna.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Sauna : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
+    public void LoylyaKiukaaseen()
+    {
+        Debug.Log("Löylyä!");
+    }
 }
